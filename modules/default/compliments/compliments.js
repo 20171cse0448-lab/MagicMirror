dc93c1908b321e24e84ab4cff6b22ec2eea81fb0
20171cse0448-lab <60284022+20171cse0448-lab@users.noreply.github.com>
2021-05-25 08:09:17 +0530

    Update compliments.js

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -8,7 +8,7 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
+			anytime: ["Hey there hope you have a nice day!"],
 			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
 			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
 			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
